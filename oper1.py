b1f844dab39240240a720b34db97326daa845b7f
S.KRISHNAN <skrish22195@gmail.com>
2017-12-20 02:42:26 +0530

    Requirements.txt

--- oper1.py
+++ oper1.py
@@ -4,7 +4,7 @@ import os
 import io
 
 df = pd.read_excel('upload_files/mass_spec_data_assgnmnt.xlsx')
-'''
+
 pc_df = df[(df['Accepted Compound ID'].str.endswith('PC',na=False)) & (df['Accepted Compound ID'].str[-3]!='L')]
 pc_df.to_csv('PC.csv',index=False)
 
@@ -23,13 +23,14 @@ def zip_files():
 		for csv_file in zip_file_list:
 			zip_file.write(csv_file)
 			os.remove(csv_file) 
+	#print(download_file.read())		
 	return download_file.getvalue()
 
 zipped_file = zip_files()
-print(zipped_file)
-'''
+print(zipped_file.decode('utf-8'))
 
 
-rounded_retention_time = df.apply(lambda row : round(row['Retention time (min)']),axis=1)
+
+'''rounded_retention_time = df.apply(lambda row : round(row['Retention time (min)']),axis=1)
 df.insert(2,'Rounded_Retention_Time',rounded_retention_time)
-df.to_csv('rounded_Retention_Time.csv',index=False)
+df.to_csv('rounded_Retention_Time.csv',index=False)'''

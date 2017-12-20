1288e3af3da3d052684092a8d20b7d5656b712a1
S.KRISHNAN <skrish22195@gmail.com>
2017-12-20 14:53:25 +0530

    process2 changes

--- uploadprocess/views.py
+++ uploadprocess/views.py
@@ -16,6 +16,8 @@ def upload(request):
 			with open('upload_files/'+filename,'wb+') as out:
 				for data in f:
 					out.write(data)
+			response = HttpResponse("Upload Success")		
+			return	response	
 	else:	
 		upload_form = FileUpload()
 	return render(request,'upload.html',{'form':upload_form})	
@@ -49,16 +51,28 @@ def process_1(request,filename):
 	return response
 
 
+def rounded_time(row):
+	time = row['Retention time (min)'] 
+	if time>1:
+		return round(time)
+	else:
+		return 1	
 def process_2(request,filename):
 	df = pd.read_excel('upload_files/'+filename+'.xlsx')
 
-	rounded_retention_time = df.apply(lambda row : round(row['Retention time (min)']),axis=1)
-	df.insert(2,'“Retention Time Roundoff (in mins)',rounded_retention_time)
+	rounded_retention_time = df.apply(lambda row : rounded_time(row),axis=1)
+	df.insert(2,'Retention Time Roundoff (in mins)',rounded_retention_time)
+
+
+	del df['m/z']
+	del df['Retention time (min)']
+
+	new_df = df.groupby(df['Retention Time Roundoff (in mins)']).mean()
 
 	response = HttpResponse(content_type='text/csv')
 	response['Content-Disposition'] = 'attachment; filename="Rounded_Retention_Time.csv"'
 
-	df.to_csv(response,index=False)
+	new_df.to_csv(response,index=True)
 
 	return response
 
